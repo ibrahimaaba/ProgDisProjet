ce220dad3e7c45f41d4fbdba9fc7d3746fe050dd
ibrahimaaba <ibrahima.bamoctar@gmail.com>
2025-01-26 16:19:05 +0100

    Docker Kubernetes Ingress operationnel

--- backend/src/App.js
+++ backend/src/App.js
@@ -6,6 +6,11 @@ app.get('/api', (req, res) => {
   res.send('Hello from Backend!');
 });
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur le backend !');
+});
+
+
+app.listen(port, '0.0.0.0',() => {
   console.log(`Backend app listening at http://localhost:${port}`);
 });
